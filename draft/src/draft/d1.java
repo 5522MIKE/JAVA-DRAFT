9b568ce89b7900bb19dd0ac792f4e482371eb904
MIKE Ming <18988774300@163.com>
2019-09-15 13:37:10 +0800

    draft

---
+++ draft/src/draft/d1.java
@@ -0,0 +1,19 @@
+package draft;
+
+public class d1 {
+
+	public static void main(String[] args) {
+//		 TODO sdf
+		int x = 1;
+		System.out.print(x);
+
+		int i8 = 123_4456_789;
+
+		float f1 = 0.8f;
+		f1 = 1.456e+5f;
+		f1 = .5f;
+
+		double d1 = 0x12.P2;
+	}
+
+}
