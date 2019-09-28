eb35fa94d28179c4b065ca41df45515493723fd0
MIKE Ming <18988774300@163.com>
2019-09-28 12:28:35 +0800

    !

--- Lab4/src/Lab4_2.java
+++ Lab4/src/Lab4_2.java
@@ -5,19 +5,40 @@
 public class Lab4_2 {
 
 	public static void main(String[] args) {
+		Integer number1 = new Integer(43);
+		Integer number2 = new Integer("43");
+		System.out.println(number1.equals(number2));
+		String s1 = number1.toString();
+		System.out.println(s1);
+		String s2[] = { "35", "67" };
+		int a = Integer.parseInt(s2[0]);
+		int b = Integer.parseInt(s2[1]);
+		int sum = a + b;
+		System.out.println(sum);
 
-		// Integer
-		Integer num = new Integer(7);
-		num = 1;
-		int i = num.intValue();
-		System.out.println(i);
+		Boolean number3 = new Boolean(true);
+		System.out.println(number3.booleanValue());
+		System.out.println(number3.equals(false));
+		System.out.println(number3.toString());
 
-		// Boolean3
-		boolean bool = new Boolean(true);
-		// Byte
-		// Character
-		// Double
-		// Number
+		byte mybyte = 43;
+		Byte n1 = new Byte(mybyte);
+		Byte n2 = new Byte("23");
+		System.out.println(n1.compareTo(n2));
+		System.out.println(n1.doubleValue());
+		System.out.println(n1.toString());
+
+		char c1 = 'A';
+		Character c2 = new Character(c1);
+		System.out.println(c2.compareTo('a'));
+		System.out.println(Character.toLowerCase(c2));
+		System.out.println(Character.isUpperCase(c2));
+
+		Double n3 = new Double(23.34);
+		System.out.println(n3.byteValue());
+		System.out.println(n3.compareTo(23.34001));
+		System.out.println(n3.toString());
+
 	}
 
 }
